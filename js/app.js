ecb77d249445ceb136653f8fd5733a434a9ac447
Amandapalacioo <133896708+Amandapalacioo@users.noreply.github.com>
2026-06-07 21:34:02 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
+// Aguarda o carregamento do DOM para rodar a primeira tela
+document.addEventListener('DOMContentLoaded', () => {
+    // A função renderLogin estará disponível pois carregaremos login.js abaixo
+    if(typeof renderLogin === 'function') {
+        renderLogin();
+    }
+});
 
