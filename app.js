a8e6e9ea7e25eff7059a8b7a17d5fe2356ef9e1e
Amandapalacioo <133896708+Amandapalacioo@users.noreply.github.com>
2026-06-07 21:34:10 -0300

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-// Aguarda o carregamento do DOM para rodar a primeira tela
-document.addEventListener('DOMContentLoaded', () => {
-    // A função renderLogin estará disponível pois carregaremos login.js abaixo
-    if(typeof renderLogin === 'function') {
-        renderLogin();
-    }
-});
