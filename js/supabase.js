bfb0c3db3aabf8dd68470256401701b5a456ae8c
Amandapalacioo <133896708+Amandapalacioo@users.noreply.github.com>
2026-06-10 21:05:43 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -4,3 +4,5 @@ const SUPABASE_ANON_KEY = 'sb_publishable_2CXAjiBSfzPwZxjHeCDMjQ_Eo36MmZqi';
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 window.supabaseClient = supabaseClient;
+
+console.log('Supabase conectado:', window.supabaseClient);
