7295a56a9c1c42e7b850d05d5c42617452b0516a
Amandapalacioo <133896708+Amandapalacioo@users.noreply.github.com>
2026-06-10 22:04:28 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://roooptrfbwenuusflkvt.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_2CXAjiBSfzPwZxjHeCDMjQ_Eo36MmZqi';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvb29wdHJmYndlbnV1c2Zsa3Z0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExMjczMDYsImV4cCI6MjA5NjcwMzMwNn0.0fC0Ao9dG3zYa7_vEf46mng6q5KyBQtJDgvCrohURNA';
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
